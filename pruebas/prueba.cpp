d3731bbe1f37eb66aa6e7acb48225c4ca866b740
julio marcano <juliocesarmarcanochacare@gmail.com>
2025-04-23 18:49:18 -0400

    Implementación de nodos e implementación de información en los mismos

--- pruebas/prueba.cpp
+++ pruebas/prueba.cpp
@@ -9,20 +9,23 @@ using namespace std;
 //creo la estructura de datos que almacenará toda la información necesaria, schedule es horario y es un vector debido a
 //la naturaleza de la información, necesitaremos ingresar numeros en string para luego transformar esos numeros a horas
 struct section{
-string name_class;
-vector<int> blocks;
-string prof;
-vector<string> students;
-int n_conflicts;
-string day;
-vector<string> schedule;
-section* next;
+    string name_class;
+    vector<int> blocks;
+    string prof;
+    vector<string> students;
+    int n_conflicts;
+    string day;
+    vector<string> schedule;
+    section* next;
 };
 
 //este es el head
 bool open_file(string name);
 void name_file();
 void clear_screen();
+void data_insert(ifstream& file, section* node);
+section* new_section(section* head);
+
 
 int main (){ 
     name_file();
@@ -35,33 +38,6 @@ void clear_screen() {
         std::system("clear");
     #endif
     }
-
-bool open_file(string name){
-    section* nodo = new section;
-    ifstream file;
-    string line;
-    file.open(name.c_str(),ios::in);
-
-    if(file.fail()){
-        return true;
-    }
-    while(!file.eof()){
-        getline(file,line);
-        if (line.find("Seccion:")!=string::npos){
-            nodo->name_class = line.substr(10);
-        }
-        else if (line.find("Bloques")!=string::npos){
-            int num;
-            string substr = line.substr(9);
-            stringstream ss(substr);
-            while(ss >> num){
-            nodo->blocks.push_back(num);
-            }
-        }
-    }
-    file.close();
-    return false;
-}
 
 void name_file(){
     clear_screen();
@@ -79,3 +55,55 @@ void name_file(){
     }
 }
 
+void data_insert(ifstream& file, section* node){
+    string line;
+    int i = 0;
+    while(!file.eof()){
+        getline(file,line);
+        if(!node->name_class.empty() && line.find("Seccion:")!=string::npos){
+            node = new_section(node);
+        }
+        if (line.find("Seccion:")!=string::npos){
+            node->name_class = line.substr(10);
+        }
+        else if (line.find("Bloques")!=string::npos){
+            int num;
+            string substr = line.substr(9);
+            stringstream ss(substr);
+            while(ss >> num){
+            node->blocks.push_back(num);
+            }
+        }
+        else if (line.find("Profesor")!=string::npos){
+            string substr = line.substr(9);
+            node->prof = substr;
+        }
+        else if(line.find("ci:")!=string::npos){
+                node->students.push_back(line);
+            }
+        }
+        cout << node->next->name_class << " ";
+        for (int block : node->next->blocks){cout << block << " ";}
+        cout << node->next->prof << " ";
+        for (string student : node->next->students) {cout << student << " ";}
+}
+
+bool open_file(string name){
+    section* nodo = new section;
+    section* cabeza = nullptr;
+    ifstream file;
+    file.open(name.c_str(),ios::in);
+    if(file.fail()){
+        return true;
+    }
+    data_insert(file, nodo);
+    file.close();
+    return false;
+}
+
+section* new_section(section* head){
+        section* node = new section;
+        node->next = head;
+        head = node;
+        return head;
+    }
